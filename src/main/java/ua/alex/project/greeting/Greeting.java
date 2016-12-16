1d49e6e067b9da8a7ff9015a3d664dcbf94dbe51
purus6233 <alkomarenko@yandex.ru>
2016-12-16 10:28:54 +0200

    add logs to project

--- src/main/java/ua/alex/project/greeting/Greeting.java
+++ src/main/java/ua/alex/project/greeting/Greeting.java
@@ -1,24 +1,35 @@
 package ua.alex.project.greeting;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.base.Preconditions;
 
 public class Greeting {
 	
+	private static Logger log = LoggerFactory.getLogger(Greeting.class);
+	
 	public String greetingBasedOnHourFinder(UserData data) throws Exception{
+		log.trace("Start finding griting based on User locale and time!");
 		Preconditions.checkState(data.validate(),
 				"Error occured during loading time and locale!");
 		if (data.getHour() > 6 & data.getHour() < 9) {
+			log.info("Loading message for morning greeting!");
 			return "Good morning, World!";
 		} else if (data.getHour() > 8 & data.getHour() < 19) {
+			log.info("Loading message for day greeting!");
 			return "Good day, World!";
 		} else if (data.getHour() > 18 & data.getHour() < 23) {
+			log.info("Loading message for evening greeting!");
 			return "Good evening, World!";
 		}
+		log.info("Loading message for night greeting!");
 		return "Good night, World!";
 	}
 	
 	public static void main(String[] args) throws Exception{
 		UserData userData = new UserData();
+		log.trace("Create instance of Greeting object!");
 		Greeting greet = new Greeting();
 		System.out.println(greet.greetingBasedOnHourFinder(userData));
 	}
--- src/main/java/ua/alex/project/greeting/UserData.java
+++ src/main/java/ua/alex/project/greeting/UserData.java
@@ -3,41 +3,52 @@ package ua.alex.project.greeting;
 import java.time.LocalDateTime;
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.base.Preconditions;
 
 public class UserData {
 	
 	private Locale locale;
 	private int hour;
-	
-	public UserData(){
+	private static Logger log = LoggerFactory.getLogger(UserData.class);
+
+	public UserData() {
 		this.locale = Locale.getDefault();
 		this.hour = LocalDateTime.now().getHour();
+		log.info("Created UserData instance with locale:" + locale
+				+ " and local time: " + hour + "!");
 	}
-	
-	public Locale getLocale() {
-		return locale;
-	}
-	public void setLocale(Locale locale) {
-		this.locale = locale;
-	}
-	public int getHour() {
-		return hour;
-	}
-	public void setHour(int hour) {
-		this.hour = hour;
-	}
-	
-	public boolean validate(){
+
+	public boolean validate() {
+		log.trace("Start validating UserData object.");
 		Preconditions.checkState(!(locale == null),
 				"The locale can not be null!");
 		Preconditions.checkState(!(hour > 23 || hour < 0),
 				"The customer hour must be in range from 0 to 23 hours!");
+		log.trace("UserData object validated.");
 		return true;
 	}
+
+	public Locale getLocale() {
+		return locale;
+	}
 	
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+	
+	public int getHour() {
+		return hour;
+	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
 	@Override
 	public String toString() {
-		return "UserLocale [locale=" + locale + ", hour=" + hour + "]";
+		return "Greeting [locale=" + locale + ", hours=" + hour + "]";
 	}
 }
