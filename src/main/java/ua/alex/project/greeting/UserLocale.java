076bd1c599bed387d9c3158c248ce140a69ca3cd
purus6233 <alkomarenko@yandex.ru>
2016-12-16 09:47:54 +0200

    add UserLocale

--- src/main/java/ua/alex/project/greeting/UserLocale.java
+++ src/main/java/ua/alex/project/greeting/UserLocale.java
@@ -1,6 +1,44 @@
 package ua.alex.project.greeting;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 public class UserLocale {
 	
-
+	private Locale locale;
+	private int hour;
+	
+	public UserLocale(){
+		this.locale = Locale.getDefault();
+		Calendar calendar = GregorianCalendar.getInstance();
+		calendar.setTime(new Date());
+		this.hour = calendar.get(Calendar.HOUR);
+	}
+	
+	public Locale getLocale() {
+		return locale;
+	}
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	
+	public boolean validate(){
+		if((locale == null) || (hour > 23 || hour < 0)){
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserLocale [locale=" + locale + ", hour=" + hour + "]";
+	}
 }
--- src/test/java/ua/alex/project/greeting/UserLocaleTest.java
+++ src/test/java/ua/alex/project/greeting/UserLocaleTest.java
@@ -1,6 +1,8 @@
 package ua.alex.project.greeting;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -17,6 +19,26 @@ public class UserLocaleTest {
 	public void test_instantiation() throws Exception {
 		UserLocale data = new UserLocale();
 		assertNotNull(data);
+	}
+	
+	@Test
+	public void test_validate_true() throws Exception {
+		UserLocale data = new UserLocale();
+		assertTrue(data.validate());
+	}
+	
+	@Test
+	public void test_validate_false_hour() throws Exception {
+		UserLocale data = new UserLocale();
+		data.setHour(24);
+		assertFalse(data.validate());
+	}
+	
+	@Test
+	public void test_validate_false_locale() throws Exception {
+		UserLocale data = new UserLocale();
+		data.setLocale(null);
+		assertFalse(data.validate());
 	}
 
 }
