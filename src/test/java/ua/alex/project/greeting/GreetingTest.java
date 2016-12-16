3b3b82511a01ac74a08abcc63bae6a3399465a32
purus6233 <alkomarenko@yandex.ru>
2016-12-16 13:02:49 +0200

    refaactoring

--- src/test/java/ua/alex/project/greeting/GreetingTest.java
+++ src/test/java/ua/alex/project/greeting/GreetingTest.java
@@ -26,37 +26,37 @@ public class GreetingTest {
 	public void greetingFinder_morning_us() throws Exception {
 		data.setLocale(Locale.US);
 		data.setHour(8);
-		assertEquals(greeting.greetingFinder(data), "Good morning, World!");
+		assertEquals("Good morning, World!", greeting.greetingFinder(data));
 	}
 	
 	@Test
 	public void greetingFinder_night_us() throws Exception {
 		data.setLocale(Locale.US);
 		data.setHour(23);
-		assertEquals(greeting.greetingFinder(data), "Good night, World!");
+		assertEquals("Good night, World!", greeting.greetingFinder(data));
 	}
 	
-	private static final Locale dLocale = new Locale.Builder().setLanguage("ru").setScript("Cyrl").build();
+	private static final Locale dLocale = new Locale.Builder().setLanguage("ru").setScript("Ru").build();
 	
 	@Test
 	public void greetingFinder_day_ru() throws Exception {
 		data.setLocale(dLocale);
 		data.setHour(10);
-		assertEquals(greeting.greetingFinder(data), "Добрый день, Мир!");
+		assertEquals("Добрый день, Мир!", greeting.greetingFinder(data));
 	}
 	
 	@Test
 	public void greetingFinder_evening_ru() throws Exception {
 		data.setLocale(dLocale);
 		data.setHour(19);
-		assertEquals(greeting.greetingFinder(data), "Добрый вечер, Мир!");
+		assertEquals("Добрый вечер, Мир!", greeting.greetingFinder(data));
 	}
 	
 	@Test (expected=IllegalStateException.class)
 	public void greetingFinder_error_locale() throws Exception {
 		data.setLocale(null);
 		data.setHour(19);
-		assertEquals(greeting.greetingFinder(data), "Добрый вечер, Мир!");
+		assertEquals("Добрый вечер, Мир!", greeting.greetingFinder(data));
 		
 	}
 	
@@ -64,7 +64,7 @@ public class GreetingTest {
 	public void greetingFinder_error_hour() throws Exception {
 		data.setLocale(Locale.ROOT);
 		data.setHour(24);
-		assertEquals(greeting.greetingFinder(data), "Добрый вечер, Мир!");
+		assertEquals("Добрый вечер, Мир!", greeting.greetingFinder(data));
 	}
 
 }
